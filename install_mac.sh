6a8cd2e1b0a503cd2b6e0648488ce44b61bb02c8
engmung <122682380+engmung@users.noreply.github.com>
2024-05-01 10:19:32 +0900

    Add files via upload

---
+++ install_mac.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Installing required libraries for the project..."
+
+pip install -r requirements.txt
+
+echo "Libraries installed successfully."
+echo "To run the Python script, type 'udp.py'
