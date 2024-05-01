fe8530cbb99e1f7bd5ff3079b0e0c4540ec512a2
engmung <122682380+engmung@users.noreply.github.com>
2024-05-01 10:18:58 +0900

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Installing required libraries for the project..."
-
-pip install -r requirements.txt
-
-echo "Libraries installed successfully."
-echo "To run the Python script, type 'udp.py'
